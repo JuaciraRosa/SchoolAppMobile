70ad44e676331d2a1f66d2e7aa9ca4307296d4d2
JUACIRA ROSA <juacira.rosa@gmail.com>
2025-07-31 22:55:42 +0100

    Add project files.

---
+++ SchoolAppMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolAppMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SchoolAppMobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolAppMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
