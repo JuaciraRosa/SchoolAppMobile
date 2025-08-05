07d3ac762c9a08904b3498b6cbef9b1137a3c339
JUACIRA ROSA <juacira.rosa@gmail.com>
2025-08-05 22:43:09 +0100

    Configure basic .NET MAUI project structure, AppShell routes, and constants

---
+++ SchoolAppMobile/Utils/ApiConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAppMobile.Utils
+{
+    public static class ApiConstants
+    {
+        public const string BaseUrl = "https://www.escolainfosysapi.somee.com/api/"; 
+    }
+}
